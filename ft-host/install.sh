fa24224684fbbfa1590e51280eed95a769f6b0f3
Allen Ye <allen.ye@ericsson.com>
2020-01-31 12:07:00 +0000

    Update

--- ft-host/install.sh
+++ ft-host/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Make beam for code patch
+mkdir -p ~/beam
+
 # Install sshpass
 wget http://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz
 tar xvzf sshpass-1.05.tar.gz
-./configure
+
 cd sshpass-1.05
+./configure
 make
 sudo make install
 
+rm -f sshpass-1.05*
