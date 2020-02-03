1c281fa3cb61b1f44f0f6867eca146790c932d19
Allen Ye <allen.ye@ericsson.com>
2020-02-03 12:28:15 +0000

    Create suite log dir

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 PATCH_BEAM_DIR=~/beam
+SUITE_LOG_DIR=~/suite_log
 
-if [ ! -d ${PATCH_BEAM_DIR}  ];then
+if [ ! -d ${PATCH_BEAM_DIR} ];then
     mkdir -p ${PATCH_BEAM_DIR}
+fi
+
+if [ ! -d ${SUITE_LOG_DIR} ];then
+    mkdir -p ${SUITE_LOG_DIR}
 fi
 
 cp env/.bash_aliases ~/.bash_aliases
