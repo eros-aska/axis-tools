321f21717e7d8fcb5392f97c97423453322a5842
Allen Ye <allen.ye@ericsson.com>
2020-02-13 08:14:22 +0000

    Add Git prompt cmd

--- install.sh
+++ install.sh
@@ -11,5 +11,13 @@ if [ ! -d ${SUITE_LOG_DIR} ];then
     mkdir -p ${SUITE_LOG_DIR}
 fi
 
-cp env/.bash_aliases ~/.bash_aliases
+cp bitbake-host/env/.bash_aliases ~/.bash_aliases
+source ~/.bash_aliases
+
+cp bitbake-host/* ~/
+
+echo "source ~/.git-prompt.sh" >> /root/.bashrc
+echo "source ~/.ps1gitrc" >> /root/.bashrc
+
+source ~/.bashrc
 source ~/.bash_aliases
