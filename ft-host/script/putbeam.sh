702ab80d6092d4ec052361b84f5fc3cd163db031
Allen Ye <allen.ye@ericsson.com>
2020-02-20 14:10:08 +0000

    Update

--- ft-host/script/putbeam.sh
+++ ft-host/script/putbeam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -r --delete -e "ssh -x" ~/beam/ root@sc:/storage/system/config/sgc/sc_dev_patches/
-rsync -r --delete -e "ssh -x" ~/beam/ root@sc:/storage/system/config/sgc/bs_SSC_1/dev_patches/
+rsync -r --delete -e "ssh -x" ~/beam/ root@SC:/storage/system/config/sgc/sc_dev_patches/
+rsync -r --delete -e "ssh -x" ~/beam/ root@SC:/storage/system/config/sgc/bs_SSC_1/dev_patches/
