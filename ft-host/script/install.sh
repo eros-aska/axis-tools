1101c122b7e8c7178f46b29746224e0a52086e16
Allen Ye <allen.ye@ericsson.com>
2020-03-19 07:20:33 +0000

    Create network when init-ft

--- ft-host/script/install.sh
+++ ft-host/script/install.sh
@@ -18,4 +18,14 @@ rm -rf ~/sshpass-1.05*
 
 # Configure passwordless login
 echo "c. Configure passwordless login"
-/local/scratch/${USER}/vsbg/tools/ssh/setup_stack_ssh.sh
+cd /local/scratch/${USER}/vsbg && ./tools/ssh/setup_stack_ssh.sh
+
+# Create Network based on FT setup
+echo "d. Create network"
+if [ `nova list | grep ers | wc -l` = 2 ]; then
+    cd /local/scratch/${USER}/vsbg && ./tools/lda_ft/setup_client_evip.sh
+    echo "ERS network created."
+else
+    cd /local/scratch/${USER}/vsbg && ./tools/lda_ft/setup_client_ipnetwork.sh
+    echo "None-ERS network created."
+fi
